cbf03cfe1f8d0a7ec1766b3ee61a6e40fe70cf8c
Dead <diwanyash1234@gmail.com>
2025-02-18 21:15:05 +0530

    test with 1000 boxes

--- App.cpp
+++ App.cpp
@@ -10,12 +10,12 @@ App::App()
 	std::uniform_real_distribution<float> adist(0.0f, 3.1415f * 2.0f);
 	std::uniform_real_distribution<float> ddist(0.0f, 3.1415f * 2.0f);
 	std::uniform_real_distribution<float> odist(0.0f, 3.1415f * 0.3f);
-	std::uniform_real_distribution<float> rdist(6.0f, 20.0f);
-	for (int i = 0; i < 80; i++)
+	std::uniform_real_distribution<float> rdist(20.0f, 300.0f);
+	for (int i = 0; i < 1000; i++)
 	{
 		boxes.push_back(std::make_unique<Box>(wnd.Gfx(), rng, adist, ddist, odist, rdist));
 	}
-	wnd.Gfx().SetProjection(DirectX::XMMatrixPerspectiveLH(1.0f, 3.0f / 4.0f, 0.5f, 40.0f));
+	wnd.Gfx().SetProjection(DirectX::XMMatrixPerspectiveLH(1.0f, 3.0f / 4.0f, 0.5f, 1000.0f));
 }
 
 int App::Go()
--- Box.cpp
+++ Box.cpp
@@ -125,5 +125,5 @@ DirectX::XMMATRIX Box::GetTransformXM() const noexcept
 	return DirectX::XMMatrixRotationRollPitchYaw(pitch, yaw, roll) *
 		DirectX::XMMatrixTranslation(r, 0.0f , 0.0f) *
 		DirectX::XMMatrixRotationRollPitchYaw(theta, phi, chi) *
-		DirectX::XMMatrixTranslation(0.0f, 0.0f, 20.0f) ;
+		DirectX::XMMatrixTranslation(0.0f, 0.0f, 400.0f) ;
 }
